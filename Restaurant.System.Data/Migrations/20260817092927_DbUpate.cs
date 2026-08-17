ebe8fb6133d97e1489b28d7f730cba10119df93c
LeeZX24 <leezixiang24@gmail.com>
2026-08-17 09:30:42 +0000

    db update

---
+++ Restaurant.System.Data/Migrations/20260817092927_DbUpate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant.System.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbUpate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
